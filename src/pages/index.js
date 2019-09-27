77958aba8349ec5a22d487c59eb3d1c749558e2a
Tom Walsh <tom.walsh@homeagency.co.uk>
2019-09-27 14:06:09 +0100

    added gatsby in develop

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+// export default () => <div>Hello Tom!</div>
+
+export default function Index(props) {
+    return (
+        <div className="about-container">
+            <p>About me.</p>
+        </div>
+    )
+}
+//  Index
